8a6fb30e5ae57c94bcc57b5185b906919b2665ae
nadznyu <nadiaalya0107@gmail.com>
2025-11-10 16:33:18 +0700

    Modifikasi Percobaan 1

--- ArrayBilangan17.java
+++ ArrayBilangan17.java
@@ -3,15 +3,10 @@ public class ArrayBilangan17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int[] bil = {5, 13,-7, 17};
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(bil[i]);
+        }
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
     }
 }
