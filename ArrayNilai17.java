9e66a5d33ee3832ecc6a922434b72bbf29e0c54e
nadznyu <nadiaalya0107@gmail.com>
2025-11-11 11:26:38 +0700

    Percobaan 2

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -8,10 +8,8 @@ public class ArrayNilai17 {
             System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus!");
-            }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
         }
     }
 }
